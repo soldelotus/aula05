effbecabfb0afa1f2bc5e19c488b0dfef42263d6
soldelotus <sdelotus@gmail.com>
2025-04-14 17:20:42 -0300

    Implementando exercício 06

--- exercicio06.py
+++ exercicio06.py
@@ -1,10 +1,19 @@
-nota1 = float(input("Digite a primeira nota: "))
-while nota1 < 0 or nota1 > 10:
-    nota1 = float(input("Valor inválido, digite novamente a primeira nota: "))
+pergunta = "S"
+while pergunta == "S":
 
-nota2 = float(input("Digite a segunda nota: "))
-while nota2 < 0 or nota2 > 10:
-    nota2 = float(input("Valor inválido, digite novamente a segunda nota: "))
+    nota1 = float(input("Digite a primeira nota: "))
+    while nota1 < 0 or nota1 > 10:
+        nota1 = float(input("Valor inválido, digite novamente a primeira nota: "))
+
+
+    nota2 = float(input("Digite a segunda nota: "))
+    while nota2 < 0 or nota2 > 10:
+        nota2 = float(input("Valor inválido, digite novamente a segunda nota: "))
+    break
 
 media = (nota1 + nota2) /2
 print(f"A média é {media}")
+pergunta = input("Deseja realizar um novo cálculo? Escreva S ou N: ")
+
+
+
