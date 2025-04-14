7b55acc3ba95383b486dbe4878685d3719e36898
soldelotus <sdelotus@gmail.com>
2025-04-14 17:14:35 -0300

    Exercício 06

---
+++ exercicio06.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digite a primeira nota: "))
+while nota1 < 0 or nota1 > 10:
+    nota1 = float(input("Valor inválido, digite novamente a primeira nota: "))
+
+nota2 = float(input("Digite a segunda nota: "))
+while nota2 < 0 or nota2 > 10:
+    nota2 = float(input("Valor inválido, digite novamente a segunda nota: "))
+
+media = (nota1 + nota2) /2
+print(f"A média é {media}")
